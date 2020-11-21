46d44f64f50631a6fd78421bd0214d316f73b781
Naponoel <lekileo@gmail.com>
2020-11-22 00:59:40 +0100

    temp branch

--- include/Atmega.h
+++ include/Atmega.h
@@ -17,20 +17,21 @@ private:
         unsigned char pinMode;
         int currentValue;
         string id;
+        string pinSignal;
     };
 public:
     vector<Pin> allPins;
-    Pin PSWM1   {18, INPUT, 0, "pinSwitchMonitor_1"};
-    Pin PSWM2   {19, INPUT, 0, "pinSwitchMonitor_2"};
-    Pin PA1     {24, INPUT, 0, "pinAmmeter_1"};
-    Pin PA2     {25, INPUT, 0, "pinAmmeter_2"};
-    Pin PR1     {5, OUTPUT, 0, "pinRelay_1"}; // todo: vrati na 14
-    Pin PR2     {13, OUTPUT, 0, "pinRelay_2"};
-    Pin PS      {4, INPUT, 0, "pinSensor"};
-    Pin PPMW1   {15, OUTPUT, 0, "pinPmw_1"};
-    Pin PPMW2   {16, OUTPUT, 0, "pinPmw_2"};
-    Pin PP1     {27, INPUT, 0, "pinPot_1"};
-    Pin PP2     {28, INPUT, 0, "pinPot_2"};
+    Pin PSWM1   {18, INPUT, 0, "pinSwitchMonitor_1", "ANALOG"};
+    Pin PSWM2   {19, INPUT, 0, "pinSwitchMonitor_2", "ANALOG"};
+    Pin PA1     {24, INPUT, 0, "pinAmmeter_1", "ANALOG"};
+    Pin PA2     {25, INPUT, 0, "pinAmmeter_2", "ANALOG"};
+    Pin PR1     {5, OUTPUT, 0, "pinRelay_1", "DIGITAL"}; // todo: vrati na 14
+    Pin PR2     {13, OUTPUT, 0, "pinRelay_2", "DIGITAL"};
+    Pin PS      {4, INPUT, 0, "pinSensor", "ANALOG"};
+    Pin PPMW1   {15, OUTPUT, 0, "pinPmw_1", "ANALOG"};
+    Pin PPMW2   {16, OUTPUT, 0, "pinPmw_2", "ANALOG"};
+    Pin PP1     {27, INPUT, 0, "pinPot_1", "ANALOG"};
+    Pin PP2     {28, INPUT, 0, "pinPot_2", "ANALOG"};
 
     Atmega() {
         allPins.push_back(PSWM1);
@@ -44,7 +45,6 @@ public:
         allPins.push_back(PPMW2);
         allPins.push_back(PP1);
         allPins.push_back(PP2);
-
     }
 
     void handleCommandRequest(const JsonObject obj) {
@@ -76,7 +76,7 @@ public:
     }
 
     void handleRequest(String message) {
-        StaticJsonDocument<60> doc;
+        StaticJsonDocument<240> doc;
         if (DeserializationError err = deserializeJson(doc, message)) {
             Serial.print("deserializeJson() failed with code: ");
             Serial.println(err.c_str());
@@ -86,23 +86,13 @@ public:
         } else if (doc["type"] == "dataRequest") {
             Serial.println("dataRequest not implemented");
         }
+        else if (doc["type"] == "dataRequest") {
+//            handleDataRequest();
+        }
+        else {
+            Serial.println("Wrong request type");
+        }
     }
 };
-
-// todo: sa sluzbene dokumentacije
-//for (JsonPair p : obj) {
-//p.key() // is a JsonString
-//p.value() // is a JsonVariant
-//}
-
-
-
-
-//void Atmega::writeNewValues(){
-//    for(unsigned int i = 0; i < allPins.size(); i++) {
-//        Serial.print(allPins[i].pinNumber);
-//        Serial.print(allPins[i].currentValue);
-//    digitalWrite(pinRelay_1.pinNumber, pinRelay_1.currentValue);
-//    }
-//}
+//{"type":"requestCommand", "pinRelay_1":1}
 #endif //ZAVRSNI_328P_ATMEGA_H
