b1316e1264b6eec4f9eea46c8b5fe8c31b872040
Naponoel <lekileo@gmail.com>
2020-11-22 00:23:33 +0100

    Weird "Got something        deserializeJson() failed with code: EmptyInput" error. This is a temporary commit

--- include/Atmega.h
+++ include/Atmega.h
@@ -17,20 +17,21 @@ private:
         unsigned char pinMode;
         int currentValue;
         string id;
+        string pinSignal;
     };
 public:
     vector<Pin> allPins;
-    Pin PSWM1   {18, INPUT, 0, "pinSwitchMonitor_1"};
-    Pin PSWM2   {19, INPUT, 0, "pinSwitchMonitor_2"};
-    Pin PA1     {24, INPUT, 0, "pinAmmeter_1"};
-    Pin PA2     {25, INPUT, 0, "pinAmmeter_2"};
-    Pin PR1     {5, OUTPUT, 0, "pinRelay_1"}; // todo: vrati na 14
-    Pin PR2     {13, OUTPUT, 0, "pinRelay_2"};
-    Pin PS      {4, INPUT, 0, "pinSensor"};
-    Pin PPMW1   {15, OUTPUT, 0, "pinPmw_1"};
-    Pin PPMW2   {16, OUTPUT, 0, "pinPmw_2"};
-    Pin PP1     {27, INPUT, 0, "pinPot_1"};
-    Pin PP2     {28, INPUT, 0, "pinPot_2"};
+    Pin PSWM1   {18, INPUT, 0, "pinSwitchMonitor_1", "ANALOG"};
+    Pin PSWM2   {19, INPUT, 0, "pinSwitchMonitor_2", "ANALOG"};
+    Pin PA1     {24, INPUT, 0, "pinAmmeter_1", "ANALOG"};
+    Pin PA2     {25, INPUT, 0, "pinAmmeter_2", "ANALOG"};
+    Pin PR1     {5, OUTPUT, 0, "pinRelay_1", "DIGITAL"}; // todo: vrati na 14
+    Pin PR2     {13, OUTPUT, 0, "pinRelay_2", "DIGITAL"};
+    Pin PS      {4, INPUT, 0, "pinSensor", "ANALOG"};
+    Pin PPMW1   {15, OUTPUT, 0, "pinPmw_1", "ANALOG"};
+    Pin PPMW2   {16, OUTPUT, 0, "pinPmw_2", "ANALOG"};
+    Pin PP1     {27, INPUT, 0, "pinPot_1", "ANALOG"};
+    Pin PP2     {28, INPUT, 0, "pinPot_2", "ANALOG"};
 
     Atmega() {
         allPins.push_back(PSWM1);
@@ -49,22 +50,40 @@ public:
 
     void handleCommandRequest(const JsonObject obj) {
         for (JsonPair keyValue : obj) {
-            for(Pin &pin: *&allPins) {
+            for(Pin& pin: *&allPins) { // todo mozda maknut &* svugdje
                 if (pin.id == keyValue.key().c_str()){
                     pin.currentValue = keyValue.value().as<int>();
-                    break;
+                    break; // todo: ovo ce mozda trebati maknuti
                 }
+            }
+        }
+//        updateOutputPins();
+    }
+
+    void updateOutputPins(){
+        for(const Pin& pin: *&allPins) {
+            if (pin.currentValue == 1 && pin.pinSignal == "DIGITAL"){
+                digitalWrite(pin.pinNumber, HIGH);
+            }
+            else if (pin.currentValue == 0 && pin.pinSignal == "DIGITAL"){
+                digitalWrite(pin.pinNumber, LOW);
+            }
+            else if (pin.pinMode == OUTPUT && pin.pinSignal == "ANALOG"){
+                analogWrite(pin.pinNumber, pin.currentValue); // Analog write [OFF-ON] = 0-255
+            }
+            else{
+                continue;
             }
         }
     }
 
-    void updateOutputPins(){
-        for(const Pin& pin: allPins) {
-            if (pin.currentValue == 1){
-                digitalWrite(pin.pinNumber, HIGH);
+    void updateInputPins(){
+        for(Pin& pin: *&allPins) {
+            if (pin.pinSignal == "DIGITAL" && pin.pinMode == INPUT){
+                pin.currentValue = (int) digitalRead(pin.pinNumber);
             }
-            else if (pin.currentValue == 0){
-                digitalWrite(pin.pinNumber, LOW);
+            else if (pin.pinSignal == "ANALOG" && pin.pinMode == INPUT){
+                pin.currentValue = (int) analogRead(pin.pinNumber);
             }
         }
     }
@@ -76,33 +95,30 @@ public:
     }
 
     void handleRequest(String message) {
-        StaticJsonDocument<60> doc;
+        StaticJsonDocument<60> doc; // todo: ovo treba izracunati kako spada
         if (DeserializationError err = deserializeJson(doc, message)) {
             Serial.print("deserializeJson() failed with code: ");
             Serial.println(err.c_str());
         }
         if (doc["type"] == "requestCommand") {
             handleCommandRequest(doc.as<JsonObject>());
-        } else if (doc["type"] == "dataRequest") {
-            Serial.println("dataRequest not implemented");
+        }
+        else if (doc["type"] == "dataRequest") {
+            handleDataRequest();
+        }
+        else {
+            Serial.println("Wrong request type");
         }
     }
+
+    void handleDataRequest() {
+        updateInputPins();
+        StaticJsonDocument<60> doc; // todo: ovo treba izracunati kako spada
+        Serial.print("sim");
+        for(Pin& pin: allPins) {
+            doc[pin.id.c_str()] = pin.currentValue;
+        }
+        serializeJson(doc, Serial);
+    }
 };
-
-// todo: sa sluzbene dokumentacije
-//for (JsonPair p : obj) {
-//p.key() // is a JsonString
-//p.value() // is a JsonVariant
-//}
-
-
-
-
-//void Atmega::writeNewValues(){
-//    for(unsigned int i = 0; i < allPins.size(); i++) {
-//        Serial.print(allPins[i].pinNumber);
-//        Serial.print(allPins[i].currentValue);
-//    digitalWrite(pinRelay_1.pinNumber, pinRelay_1.currentValue);
-//    }
-//}
 #endif //ZAVRSNI_328P_ATMEGA_H
